5533aabe9f8d23e81507bde50cc2d9959c654183
Stan <99252047+Stanpac@users.noreply.github.com>
2026-07-02 13:42:17 +0200

    Fix

---
+++ Source/Brainless/Character/BrainlessPlayerController.cpp
@@ -0,0 +1,33 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "BrainlessPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
+#include "InputMappingContext.h"
+#include "Blueprint/UserWidget.h"
+#include "Brainless.h"
+#include "Widgets/Input/SVirtualJoystick.h"
+
+void ABrainlessPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABrainlessPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	// only add IMCs for local player controllers
+	if (IsLocalPlayerController())
+	{
+		// Add Input Mapping Contexts
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+		{
+			for (UInputMappingContext* CurrentContext : DefaultMappingContexts)
+			{
+				Subsystem->AddMappingContext(CurrentContext, 0);
+			}
+		}
+	}
+}
